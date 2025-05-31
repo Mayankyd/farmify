a91c0147f938585b04c43c0cfcce345239aa731e
Mayank Yadav <mayankyd2110@gmail.com>
2025-06-01 02:45:59 +0530

    changed port

--- app.py
+++ app.py
@@ -137,5 +137,5 @@ def dis_predict():
 
 # ==================== Run App ====================
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5003))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
